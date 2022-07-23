0fb31ba6bdbe25d6abccc332dbbeba5e733e1225
MattRiley78 <bassfacer@yahoo.com>
2022-07-22 21:32:37 -0500

    Retirement Info added.

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/schema.sql
@@ -59,3 +59,25 @@ SELECT * FROM dept_manager;
 SELECT * FROM salaries;
 SELECT * FROM dept_emp;
 SELECT * FROM titles;
+
+--RETIREMENT ELIGIBILITY
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+from employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Retirement Info Table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+select * from retirement_info;
+
